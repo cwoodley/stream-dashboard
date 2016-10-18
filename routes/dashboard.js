4018941c5aaee4394ad18ed145bb4a8f18a989fe
Cale Woodley <cale@loadedcommunications.com.au>
2016-10-18 15:46:07 +0800

    added timer

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,7 +7,6 @@ router.get('/', function(req, res, next) {
   res.render(
     'dashboard',
     {
-      title : 'Dashboard',
     }
   );
 });
@@ -15,6 +14,17 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   console.log(req.body);
   res.io.emit("currentGame", req.body.currentGame);
+  res.io.emit("nextGame", req.body.nextGame);
+  res.io.emit("timerFinish", req.body.timerFinish)
+
+  res.render('dashboard',{
+    title : 'Dashboard',
+    currentGame: req.body.currentGame,
+    nextGame: req.body.nextGame,
+    timeEnd: req.body.timeEnd,
+    dateEnd: req.body.dateEnd,
+    timerFinish: req.body.timerFinish,
+  });
 });
 
 module.exports = router;
