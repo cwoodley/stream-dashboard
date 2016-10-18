06d5e39fb86cc4ed77315fec39f4b74a11887ff4
Cale Woodley <cale@loadedcommunications.com.au>
2016-10-18 12:10:39 +0800

    sockets working

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./database');
-var methodOverride = require('method-override');
-
-var swig = require('swig');
 
 var routes = require('./routes/index');
+var users = require('./routes/users');
 var dashboard = require('./routes/dashboard');
 
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-var swig = new swig.Swig();
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-
-app.use(methodOverride('_method'))
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -30,7 +25,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next){
+  res.io = io;
+  next();
+});
+
 app.use('/', routes);
+app.use('/users', users);
 app.use('/dashboard', dashboard);
 
 // catch 404 and forward to error handler
@@ -65,4 +66,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+module.exports = {app: app, server: server};
