3f0e1b836f860acc19145edfcd1fa4cee80411b0
Cale Woodley <inbox@cale.io>
2016-10-21 13:10:12 +0800

    Updating Timer component

--- src/components/panels/Timer/Timer.js
+++ src/components/panels/Timer/Timer.js
@@ -1,6 +1,36 @@
 var React = require('react');
+var DatePicker = require('react-datepicker');
+var TimePicker = require('rc-time-picker');
+var moment = require('moment');
+
+
+require('react-datepicker/dist/react-datepicker.css');
+require('rc-time-picker/assets/index.css');
 
 var Timer = React.createClass({
+  handleInputChange: function(e) {
+    console.log(e.target.value)
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    formData = this.formData;
+
+    console.log(formData);
+
+    fetch('http://localhost:3000/update',{
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData),
+    }).then(function(response){
+      console.log(response.status)
+    }).catch(function(err) {
+      console.log(err)
+    });
+  },
   render: function(){
     return (
       <form onSubmit={this.handleSubmit}>
@@ -8,13 +38,13 @@ var Timer = React.createClass({
           <div className="col-lg-6">
             <div className="input-group">
               <label>End Time</label>
-              <input type="text" className="form-control" placeholder="" ref="endTime" />
+              <TimePicker showSecond={false} format="HH:mm" name="timeEnd" onChange={this.handleInputChange} />
             </div>
           </div>
           <div className="col-lg-6">
             <div className="input-group">
               <label>End Date</label>
-              <input type="text" className="form-control" placeholder="" ref="endDate" />
+              <DatePicker dateFormat="YYYY-DD-MM" name="dateEnd" onChange={this.updateFormData} />
             </div>
           </div>
         </div>
