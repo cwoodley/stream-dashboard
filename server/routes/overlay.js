bd4c631fdf0cc3cad9781f3e0fbad108dfd2e848
Cale Woodley <inbox@cale.io>
2017-05-12 10:54:47 +0800

    Added a basic default theme file if no theme has been manually set

--- server/routes/overlay.js
+++ server/routes/overlay.js
@@ -5,8 +5,16 @@ var router = express.Router();
 /* GET overlay page. */
 router.get('/', function(req, res, next) {
 
+  var theme;
+
+  if (!process.env.OVERLAY_THEME) {
+    theme = "default"
+  } else {
+    theme = process.env.OVERLAY_THEME
+  }
+
   res.render(
-    path.join(__dirname, '../../client/overlay/themes/' + process.env.OVERLAY_THEME + '/overlay.html'),{
+    path.join(__dirname, '../../client/overlay/themes/' + theme + '/overlay.html'),{
       title: 'Overlay'
     }
 
