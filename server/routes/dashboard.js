5f2f3b0d55c580bdea93c8453da47da0f7f6f27c
Cale Woodley <inbox@cale.io>
2018-05-04 22:15:10 +0800

    Updates dashboard route to send data

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render(
-    path.join(__dirname, '../../client/dashboard/dashboard.html'),{
+    path.join(__dirname, '../views/dashboard/dashboard.html'),{
       title: 'Dashboard'
     }
   );
--- server/routes/update.js
+++ server/routes/update.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  var emitters = [
-    {text: "hi"}
-  ]
 
-  for (var i in emitters) {
-    if (emitters.hasOwnProperty(i)) {
-      console.log('sending ' + "text" + " => " + emitters[i].text);
-      res.io.emit("text", emitters[i])
+  // res.io.emit("currentGame", 'can you hear me?', 1, 2, 'abc')
+  
+  res.end();
+});
+
+router.post('/', function(req, res, next) {
+  const formData = req.body
+  const emitters = []
+
+  /** 
+   * Converts form data into array of objects
+   */
+  for (var i in formData) {
+    if (formData.hasOwnProperty(i)) {
+      emitters.push({label: i, data: formData[i]})
     }
   }
 
-
-  res.end();
-});
+  /**
+   * Send data to client
+   */
+  emitters.map(emitter => {
+    res.io.emit(emitter.label, emitter.data)
+  })
+  
+  res.redirect('./dashboard');
+})
 
 module.exports = router;
