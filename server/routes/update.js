df9e720fea819bcb38b5da66f96a3221a2ddc4f6
Cale Woodley <inbox@cale.io>
2018-05-05 12:06:33 +0800

    Adds webscaledb support

--- server/routes/update.js
+++ server/routes/update.js
@@ -1,5 +1,14 @@
 import express from "express"
+import * as db from 'webscaledb'
+import * as path from 'path'
+
 const router = express.Router();
+
+const DB_NAME = path.join(process.cwd(), 'data.json')
+
+function saveData(data) {
+  db.backup(DB_NAME)
+}
 
 router.get('/', function(req, res, next) {  
   res.end();
@@ -13,16 +22,20 @@ router.post('/', function(req, res, next) {
    * Converts form data into array of objects
    */
   for (var i in formData) {
-    if (formData.hasOwnProperty(i)) {
+    if (formData[i]) {
       emitters.push({label: i, data: formData[i]})
     }
   }
 
   /**
-   * Send data to client
+   * Send data to client & webascaledb
    */
   emitters.map(emitter => {
     res.io.emit(emitter.label, emitter.data)
+    
+    saveData(db.set(emitter.label, emitter.data))
+    console.log(emitter.label,emitter.data);
+
     console.log("sending", emitter.label, emitter.data);
   })
   
